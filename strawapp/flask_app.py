54cfe31578f848ac9900ff46ead91f33e5af2d41
Nahuel Ambrosini <nambrosini@onapsis.com>
2020-08-11 18:48:52 -0300

    ⚠  Enable CORS to allow graphql-rover to reach the localhost server with the API.

--- strawapp/flask_app.py
+++ strawapp/flask_app.py
@@ -1,9 +1,15 @@
 from .flask_view import GraphQLAPI
 from flask import Flask
+from flask_cors import CORS
 
 from .app import schema
 
 app = Flask(__name__, static_folder='static')
+
+# NOT SECURE AT ALL!! don't copy paste!
+CORS(app, origins='null') # <-- Danger
+# I did this just to allow graphql-rover to introspect the schema in a LOCAL environment.
+# See https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#Preflighted_requests for why you be careful with CORS
 
 
 app.add_url_rule(
