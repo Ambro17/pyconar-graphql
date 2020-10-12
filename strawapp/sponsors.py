45b49a3bcad24809b99e88dc2259115451cfbb46
Ambro17 <ambro17.1@gmail.com>
2020-10-12 00:00:25 -0300

    🔧 Fix mapping from db to domain model

--- strawapp/sponsors.py
+++ strawapp/sponsors.py
@@ -1,22 +1,25 @@
 from strawapp.domain import Company, Sponsor, OpenPosition, SponsorType
 from typing import List
-from strawapp.db.database import Sponsorship
+from strawapp.db.database import Sponsorship, OpenPosition as Job
 
 CATEGORIES = {
     1: SponsorType.DIAMANTE,
     2: SponsorType.ORO,
 }
 
+def to_company(company):
+    return Company(
+        name=company.name,
+        tagline=company.tagline,
+        website=company.website,
+        technologies=company.technologies.split(','),
+        open_positions=company.open_positions
+    )
+
 def get_sponsors() -> List[Sponsor]:
     return [
         Sponsor(
-            company=Company(
-                name=s.company.name,
-                tagline=s.company.tagline,
-                website=s.company.website,
-                technologies=s.company.technologies.split(','),
-                open_positions=s.company.open_positions
-            ),
+            company=to_company(s.company),
             category=CATEGORIES[s.category]
         )
         for s in Sponsorship.select()
@@ -25,6 +28,10 @@ def get_sponsors() -> List[Sponsor]:
 
 def get_open_opportunities() -> List[OpenPosition]:
     return [
-        s.company.open_positions
-        for s in Sponsorship.select()
+        OpenPosition(
+           company=to_company(job.company),
+           title=job.title,
+           url=job.url,
+        )
+        for job in Job.select()
     ]
