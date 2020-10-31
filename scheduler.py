e614e7909c4df42cb2d5b3ca5055b4ff269c4378
Ambro17 <ambro17.1@gmail.com>
2020-10-31 03:43:32 -0300

    🎨 Prettify script

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,6 @@ import json
 from playwright import sync_playwright
 from dateutil.parser import parse
 
-charlas = []
 
 def parse_dates(dia:str, start_hour: str, end_hour: str):
     """Build datetime from day string plus hour and minute string """
@@ -19,47 +18,76 @@ def parse_dates(dia:str, start_hour: str, end_hour: str):
     return start_datetime, end_datetime
 
 
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.newPage()
+def get_talks_schedule():
+    charlas = []
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.newPage()
 
-    # Visit Schedule Page
-    page.goto('https://eventos.python.org.ar/events/pyconar2020/schedule')
+        # Visit Schedule Page
+        page.goto('https://eventos.python.org.ar/events/pyconar2020/schedule')
 
-    # Get all divs with the schedule of the day
-    days = page.querySelectorAll('css=div .well')
-    for day in days:
-        # Get the day header text
-        dia = day.querySelector('h2').innerText().strip()
-        print(dia)
+        # Get all divs with the schedule of the day
+        days = page.querySelectorAll('css=div .well')
+        for day in days:
+            # Get the day header text
+            dia = day.querySelector('h2').innerText().strip()
+            print(dia)
 
-        dia = dia.split('- ')[1] # Día 1 - 16/11/2020 --> 16/11/2020
-        # Get all talks from that day.
-        charlas_divs = day.querySelectorAll('css=div .fc-content')
-        for charla in charlas_divs:
-            """
-            Expected html
-                <div class="fc-time" data-start="19:00" data-full="7:00 PM - 8:00 PM">
-                    <span>19:00 - 20:00</span>
-                </div>
-                <div class="fc-title">¿Por que no se hacen mas tests?</div>
-            """
-            # For each talk, get its schedule and its title
-            horario = charla.querySelector('.fc-time').innerText()
-            start, end = horario.split(' - ')
-            start_date, end_date = parse_dates(dia, start, end)
+            dia = dia.split('- ')[1] # Día 1 - 16/11/2020 --> 16/11/2020
+            # Get all talks from that day.
+            charlas_divs = day.querySelectorAll('css=div .fc-content')
+            for charla in charlas_divs:
+                """
+                Expected html
+                    <div class="fc-time" data-start="19:00" data-full="7:00 PM - 8:00 PM">
+                        <span>19:00 - 20:00</span>
+                    </div>
+                    <div class="fc-title">¿Por que no se hacen mas tests?</div>
+                """
+                # For each talk, get its schedule and its title
+                horario = charla.querySelector('.fc-time').innerText()
+                start, end = horario.split(' - ')
+                start_date, end_date = parse_dates(dia, start, end)
 
-            titulo = charla.querySelector('.fc-title').innerText()
-            print('Charla ', titulo)
-            charlas.append({
-                'day': str(dia),
-                'titulo': titulo,
-                'start': start_date.isoformat(),
-                'end': end_date.isoformat(),
-            })
+                titulo = charla.querySelector('.fc-title').innerText()
+                print('Charla ', titulo)
+                charlas.append({
+                    'day': str(dia),
+                    'titulo': titulo,
+                    'start': start_date.isoformat(),
+                    'end': end_date.isoformat(),
+                })
 
-    browser.close()    
+        browser.close()    
+
+    return charlas
 
 
-with open('./pyconar/data/2020_schedule.json', 'w', encoding='utf-8') as f:
-    json.dump(charlas, f, indent=4, ensure_ascii=False)
+def dump_talks_with_schedule_info(schedule):
+    with open('pyconar/data/charlas.json', 'r') as f:
+        all_charlas = json.load(f)
+
+    title_to_charla = {charla['titulo']: charla for charla in schedule}
+
+    for charla in all_charlas['2020']:
+        # Add schedule info
+        name = charla['title']
+        schedule_of_charla = title_to_charla[name]
+        charla['schedule'] = {
+            'time': schedule_of_charla['day'],
+            'start': schedule_of_charla['start'],
+            'end': schedule_of_charla['end'],
+        }
+
+    nextTalks = all_charlas['2020']
+    nextTalks = sorted(nextTalks, key=lambda x: x['schedule']['start'])
+
+    assert len(nextTalks) == len(schedule), 'Not all charlas were scheduled'
+
+    with open('pyconar/data/2020_schedule.json', 'w') as f:
+        json.dump(nextTalks , f, indent=4, ensure_ascii=False)
+
+
+charlas_schedule = get_talks_schedule()
+dump_talks_with_schedule_info(charlas_schedule)
